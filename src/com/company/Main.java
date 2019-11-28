882e326a896fe01edc6ddd16deda97d2d670c846
Armen <mingbo14@gmail.com>
2019-11-28 09:09:33 +0400

    Initial testings #2

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static int a;
+public static int b;
+
+    public static void main(String[] args) {
+        a = 9;
+        b = 10;
+	int c = a+b;
+	System.out.println(c);// write your code here
+    }}
